6e759bb464ecfdc0942837d086e6f9d0a1793398
duong-vo <vodh2@miamioh.edu>
2022-10-23 23:45:32 -0400

    got database to work

--- server.py
+++ server.py
@@ -19,6 +19,15 @@ name_list = [
     }
     ]
 
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('names.sqlite')
+    except sqlite3.error as e:
+        print(e)
+    return conn
+
+
 @app.route('/')
 def index():
     return "Hello World"
@@ -29,51 +38,62 @@ def names():
     conn = db_connection()
     cursor = conn.cursor()
     if request.method == 'GET':
-        if len(name_list) > 0:
-            return jsonify(name_list)
+        cursor = conn.execute("SELECT * FROM name")
+        names = [
+            dict(id=row[0], name=row[1], age=row[2])
+            for row in cursor.fetchall()
+        ]
+        if names is not None:
+            return jsonify(names)
         else:
             "Nothing found", 404
     if request.method == 'POST':
         new_name = request.form['name']
         new_age = request.form['age']
-        iD = name_list[-1]['id'] + 1
-
-        new_obj = {
-            'id': iD,
-            'name': new_name,
-            'age': new_age
-        }
-        name_list.append(new_obj)
-        return jsonify(new_obj), 201
+        sql = """INSERT INTO name (name, age)
+                 VALUES (?, ?)"""
+        cursor = cursor.execute(sql, (new_name, new_age))
+        conn.commit()
+        
+        return "name with the id: {cursor.lastrowid} created successfuly", 201
 
 
 # GET/PUT/DELETE single name
 @app.route('/names/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def single_name(id):
+    conn = db_connection()
+    cursor = conn.cursor()
+    name = None
     if request.method == 'GET':
-        for name in name_list:
-            if name['id'] == id:
-                return jsonify(name)
-            pass
+        cursor.execute("SELECT * FROM name WHERE id=?", (id,))
+        rows = cursor.fetchall()
+        for r in rows:
+            name = r
+        if name is not None:
+            return jsonify(name), 200
+        else:
+            return "Sum-ting wong", 404
     elif request.method == 'PUT':
-        for name in name_list:
-            if name['id'] == id:
-                new_name = request.form['name']
-                new_age = request.form['age']
-                iD = name_list[-1]['id'] + 1
+        sql = """UPDATE name
+                    SET name=?,
+                        age=?,
+                    WHERE id=? """
+        new_name = request.form['name']
+        new_age = request.form['age']
 
-                new_obj = {
-                    'id': iD,
-                    'name': new_name,
-                    'age': new_age
-                }
-                name_list.append(new_obj)
-                return jsonify(new_obj), 201
+        new_obj = {
+                'id': iD,
+                'name': new_name,
+                'age': new_age
+            }
+        conn.execute(sql, (author, language, title, id))
+        conn.commit()
+        return jsonify(new_obj), 201
     elif request.method == 'DELETE':
-        for index, name in enumerate(name_list):
-            if name['id'] == id:
-                name_list.pop(index)
-                return jsonify(name_list)
+        sql = """ DELETE FROM name WHERE id=? """
+        conn.execute(sql, (id,))
+        conn.commit()
+        return "The book with has been deleted", 200
 
 
 if __name__ == '__main__':
